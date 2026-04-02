a0ee85c5d001c782325e12d397ade02971a71d9b
Kritik <kritik@Kritiks-MacBook-Pro.local>
2026-04-02 15:32:22 +0530

    removed logs

--- dist/consumer/SqsConsumer.js
+++ dist/consumer/SqsConsumer.js
@@ -88,13 +88,13 @@ let SqsConsumer = class SqsConsumer {
         catch (err) {
             const error = err;
             //$log.error("[event-broker] SQS ReceiveMessage failed, will retry on next poll",this.config.sqs.queueUrl)
-            $log.warn("[event-broker] SQS ReceiveMessage failed, will retry on next poll", {
-                queueUrl: this.config.sqs.queueUrl,
-                serviceName: this.serviceName,
-                error: error?.message,
-                name: error?.name,
-            });
-            console.log({ error });
+            // $log.warn("[event-broker] SQS ReceiveMessage failed, will retry on next poll", {
+            //   queueUrl: this.config.sqs.queueUrl,
+            //   serviceName: this.serviceName,
+            //   error: error?.message,
+            //   name: error?.name,
+            // });
+            // console.log({error});
         }
         if (this.polling) {
             this.pollTimeoutId = setTimeout(() => this.poll(), 0);
